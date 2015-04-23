c97626bfb7c38c805ffacf77d88dfe20de3e8450
brol <brol31@gmail.com>
2015-04-23 18:45:00 +0200

    1.5

---
+++ _define.php
@@ -0,0 +1,28 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of lunarPhase, a plugin for Dotclear.
+# 
+# Copyright (c) 2009-2015 Tomtom
+# Contributor: Pierre Van Glabeke
+#
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
+
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */			     'lunarPhase',
+	/* Description */		 'Display the moon phases',
+	/* Author */			   'Tomtom, Pierre Van Glabeke',
+	/* Verion */			   '1.5',
+	/* Properties */
+	array(
+		'permissions' => 'usage,contentadmin',
+		'type' => 'plugin',
+		'dc_min' => '2.7',
+		'support' => 'http://lab.dotclear.org/wiki/plugin/lunarPhase',
+		'details' => 'http://plugins.dotaddict.org/dc2/details/lunarPhase'
+	)
+);
---
+++ _widgets.php
@@ -0,0 +1,44 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of lunarPhase, a plugin for Dotclear.
+#
+# Copyright (c) 2009-2015 Tomtom
+# Contributor: Pierre Van Glabeke
+#
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
+if (!defined('DC_RC_PATH')) { return; }
+
+$core->addBehavior('initWidgets',array('lunarPhaseWidgets','initWidgets'));
+
+class lunarPhaseWidgets
+{
+	public static function initWidgets($w)
+	{
+		$w->create('lunarphase',__('LunarPhase: moon phases'),array('lunarPhasePublic','widget'),
+			null,
+			__('Display the moon phases'));
+		$w->lunarphase->setting('title',__('Title:'),__('Moon phases'));
+		$w->lunarphase->setting('phase',__('Display actual phase of moon'),1,'check');
+		$w->lunarphase->setting('illumination',__('Display actual illumination of moon'),1,'check');
+		$w->lunarphase->setting('age',__('Display actual age of moon'),1,'check');
+		$w->lunarphase->setting('dist_to_earth',__('Display actual distance between moon and earth'),1,'check');
+		$w->lunarphase->setting('dist_to_sun',__('Display actual distance between moon and sun'),1,'check');
+		$w->lunarphase->setting('moon_angle',__('Display actual angle of moon'),1,'check');
+		$w->lunarphase->setting('sun_angle',__('Display actual angle of sun'),1,'check');
+		$w->lunarphase->setting('parallax',__('Display actual parallax of moon'),1,'check');
+		$w->lunarphase->setting('previsions',__('Display all previsions for the next moon phases'),1,'check');
+		$w->lunarphase->setting('homeonly',__('Display on:'),0,'combo',
+			array(
+				__('All pages') => 0,
+				__('Home page only') => 1,
+				__('Except on home page') => 2
+				)
+		);
+    $w->lunarphase->setting('content_only',__('Content only'),0,'check');
+    $w->lunarphase->setting('class',__('CSS class:'),'');
+		$w->lunarphase->setting('offline',__('Offline'),0,'check');
+	}
+}
