ce856920457bfe068f64814cbb83ac5aabb2e1dc
Jean-Christian Denis <contact@jcdenis.fr>
2023-05-08 10:40:07 +0200

    harmonize My class

--- src/My.php
+++ src/My.php
@@ -17,13 +17,10 @@ namespace Dotclear\Plugin\lunarPhase;
 use dcCore;
 
 /**
- * Plugin definitions
+ * This module definitions.
  */
 class My
 {
-    /** @var    string  Required php version */
-    public const PHP_MIN = '7.4';
-
     /** @var    array   List of lunar phase => image */
     public const LUNAR_PHASES = [
         'new_moon'             => 'nm.png',
@@ -36,8 +33,11 @@ class My
         'waning_crescent_moon' => 'wcm2.png',
     ];
 
+    /** @var    string  This module required php version */
+    public const PHP_MIN = '7.4';
+
     /**
-     * This module id
+     * This module id.
      */
     public static function id(): string
     {
@@ -45,15 +45,25 @@ class My
     }
 
     /**
-     * This module name
+     * This module name.
      */
     public static function name(): string
     {
-        return __((string) dcCore::app()->plugins->moduleInfo(self::id(), 'name'));
+        $name = dcCore::app()->plugins->moduleInfo(self::id(), 'name');
+
+        return __(is_string($name) ? $name : self::id());
     }
 
     /**
-     * Check php version
+     * This module path.
+     */
+    public static function path(): string
+    {
+        return dirname(__DIR__);
+    }
+
+    /**
+     * Check this module PHP version compliant.
      */
     public static function phpCompliant(): bool
     {
